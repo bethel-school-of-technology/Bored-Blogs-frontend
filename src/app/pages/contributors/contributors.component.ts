23edeac2f62b4dc83c47955ef3c07d8ee06f967e
jacob <jst4nt0n@gmail.com>
2020-04-23 16:04:51 -0400

    good start on contrib

--- src/app/pages/contributors/contributors.component.ts
+++ src/app/pages/contributors/contributors.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit } from '@angular/core';
 *
 */
 
-
+function make(mName,mBio,mColor) {
+  return {
+    name:mName,
+    bio:mBio,
+    color:mColor
+  }
+};
 
 @Component({
   selector: 'app-contributors',
@@ -11,7 +17,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contributors.component.scss']
 })
 export class ContributorsComponent implements OnInit {
-
+  
+  //TODO: replace hard coded data with services
+  contributors = [
+    {//hard coded data
+      name:"Jacob",
+      bio:"Hello my name is Jacob I like to program",
+      //color gets used by the ngstyle directive
+      //TODO: validate data so it that it dosen't kill angular
+      color:{"border":"4px solid blue"}
+    },
+    make("jackie","jackie likes sushi",{"border":"4px solid red "}),
+  ];
   constructor() { }
 
   ngOnInit() {
