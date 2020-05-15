6760d911ce0a52d09bb53b1a86726b3314111c0b
jake <jst4nt0n@gmail.com>
2020-05-15 14:02:45 -0400

    fixed errors

--- src/app/pages/all-post/post-comment/post-comment.component.ts
+++ src/app/pages/all-post/post-comment/post-comment.component.ts
@@ -1,29 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Comment } from '../../../models/comment';
-import { PostCommentService } from '../../../services/post-comment.service';
+import { Component, OnInit, Input } from "@angular/core";
+import { Router } from "@angular/router";
+import { Comment } from "../../../models/comment";
+import { PostCommentService } from "../../../services/post-comment.service";
 
 @Component({
-  selector: 'app-post-comment',
-  templateUrl: './post-comment.component.html',
-  styleUrls: ['./post-comment.component.scss']
+  selector: "app-post-comment",
+  templateUrl: "./post-comment.component.html",
+  styleUrls: ["./post-comment.component.scss"],
 })
-
 export class PostCommentComponent implements OnInit {
-@Input()
-  newComment: Comment;
+  @Input() newComment: Comment;
   isAdmin = false;
 
-  addComment() {
+  addComment():void {
     this.commentDataService
-    .addComment(this.newComment)
-    .subscribe(p => this.router.navigate(['/post-detail']));
+      .addComment(this.newComment)
+      .subscribe((p) => this.router.navigate(["/post-detail"]));
   }
 
+  constructor(
+    private commentDataService: PostCommentService,
+    private router: Router
+  ) {}
 
-  constructor(private commentDataService: PostCommentService, private router: Router) { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/services/post-comment.service.ts
+++ src/app/services/post-comment.service.ts
@@ -1,52 +1,70 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Comment } from '../models/comment';
-import { Config } from '../config/config';
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Comment } from "../models/comment";
+import { Config } from "../config/config";
 
-const comments: Comment[] = [
-  new Comment(1, 'some user', 'i like turtles')
-];
+var comments: Comment[] = [new Comment(1, "some user", "i like turtles")];
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class PostCommentService {
-
   url: string = Config.apiUrl;
 
   getComments(): Observable<Comment[]> {
-   // return this.http.get<Post[]>(this.url + 'posts');
-    return new Observable((observer => { var copy = [...comments]; observer.next(copy); }));
+    if (Config.weAreUsingCloud) {
+      return this.http.get<Comment[]>(this.url + "fixme"); //TODO:FIXME  the url needs to be updated and we need to add token to headers
+    } else {
+      return new Observable((observer) => {
+        var copy = [...comments];
+        observer.next(copy);
+      });
+    }
   }
 
   addComment(comment: Comment): Observable<Comment[]> {
- //   return this.http.post<Comment[]>(this.url + 'post-detail', comment);
- return new Observable((observer => { comments.push(comment); var copy = [...comments]; observer.next(copy); }));
+    if (Config.weAreUsingCloud) {
+      return this.http.post<Comment[]>(this.url + "post-detail", comment);
+    } else {
+      return new Observable((observer) => {
+        comments.push(comment);
+        var copy = [...comments];
+        observer.next(copy);
+      });
+    }
   }
 
-  updateComment(comment:Comment):Observable<Comment[]>{
-    return new Observable((observer => {
+  updateComment(comment: Comment): Observable<Comment[]> {
+    if (Config.weAreUsingCloud) {
+        //TODO: needs the http thingy
+    } else {
+      return new Observable((observer) => {
         for (var i = 0; i < comments.length; i++) {
-      //      look through the array and replace the item
-            if (comment.id == i) {
-              comments[i] = comment;
-              i = comments.length;//we found what we wanted we can stop
-            }
+          //      look through the array and replace the item
+          if (comment.id == i) {
+            comments[i] = comment;
+            i = comments.length; //we found what we wanted we can stop
           }
-          var copy = [...comments];
-          observer.next(copy);
-        }));
+        }
+        var copy = [...comments];
+        observer.next(copy);
+      });
+    }
+    
   }
 
   deleteComment(id: number): Observable<Comment[]> {
-  //  return this.http.delete<Comment[]>(this.url + 'post-detail' + id);
-  this.postDataService.deletePost(id).subscribe((p) => {
-    (this.comments = p)
-  });
+    if (Config.weAreUsingCloud) {
+      return this.http.delete<Comment[]>(this.url + 'post-detail' + id);
+    } else {
+      return new Observable((observer) => {
+        comments = comments.filter((p) => p.id != id);
+        var copy = [...comments];
+        observer.next(copy);
+      });
+    }
   }
 
-  
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 }
