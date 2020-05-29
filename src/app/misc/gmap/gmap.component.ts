9c5dbce5e71371202163ef80071abfd92fd04a5c
jake <jst4nt0n@gmail.com>
2020-05-29 15:40:22 -0400

    build fails

---
+++ src/app/misc/gmap/gmap.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { GoogleMapsModule } from "@angular/google-maps";
+
+
+//AIzaSyCyUj8Iop26HzwdeMKyKH6CxEwoZe0VROk
+@Component({
+  selector: 'gmap',
+  templateUrl: './gmap.component.html',
+  styleUrls: ['./gmap.component.scss']
+})
+export class GmapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/misc/misc.module.ts
+++ src/app/misc/misc.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { AlertsComponent } from './alerts/alerts.component';
+import { GmapComponent } from './gmap/gmap.component';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 
 @NgModule({
-  declarations: [NavbarComponent, AlertsComponent],
+  declarations: [NavbarComponent, AlertsComponent, GmapComponent],
   imports: [
+    GoogleMapsModule,
     CommonModule,
     AppRoutingModule
   ],
