cffd6ff02ed85d00a9a1a9282ec4fe22ab768317
jake <jst4nt0n@gmail.com>
2020-06-01 15:05:07 -0400

    stuff

--- src/app/misc/gmap/gmap.component.ts
+++ src/app/misc/gmap/gmap.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { GoogleMapsModule } from "@angular/google-maps";
 
 
 //AIzaSyCyUj8Iop26HzwdeMKyKH6CxEwoZe0VROk
--- src/app/misc/misc.module.ts
+++ src/app/misc/misc.module.ts
@@ -1,19 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './navbar/navbar.component';
-import { AppRoutingModule } from '../app-routing.module';
-import { AlertsComponent } from './alerts/alerts.component';
-import { GmapComponent } from './gmap/gmap.component';
-import { GoogleMapsModule } from '@angular/google-maps';
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { AppRoutingModule } from "../app-routing.module";
+import { AlertsComponent } from "./alerts/alerts.component";
+import { GmapComponent } from "./gmap/gmap.component";
 
 @NgModule({
   declarations: [NavbarComponent, AlertsComponent, GmapComponent],
-  imports: [
-    GoogleMapsModule,
-    CommonModule,
-    AppRoutingModule
-  ],
-  exports: [NavbarComponent, AlertsComponent]
+  imports: [CommonModule, AppRoutingModule],
+  exports: [NavbarComponent, AlertsComponent],
 })
-export class MiscModule { }
+export class MiscModule {}
